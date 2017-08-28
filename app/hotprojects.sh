63fc6de8388540ebe356f0d6790ebe58a4254cee
rrequero <rrequero@gmail.com>
2017-08-28 17:19:24 +0200

    modify hotprojects and include node

--- app/hotprojects.sh
+++ app/hotprojects.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -ex
 
-./hotprojects.js --raw > hotprojects.raw.geojson
+node ./hotprojects.js --raw > hotprojects.raw.geojson
 echo "Executing mapbox-tile-copy"
-../node_modules/@mapbox/mapbox-tile-copy/bin/mapbox-tile-copy.js --minzoom=1 --maxzoom=9 hotprojects.raw.geojson s3://osma/tiles/{z}/{x}/{y}.pbf
+node ../node_modules/@mapbox/mapbox-tile-copy/bin/mapbox-tile-copy.js --minzoom=1 --maxzoom=9 hotprojects.raw.geojson s3://osma/tiles/{z}/{x}/{y}.pbf
 rm hotprojects.raw.geojson
 
-./hotprojects.js > hotprojects.geojson
+node ./hotprojects.js > hotprojects.geojson
 gzip -9 hotprojects.geojson
 aws s3 cp hotprojects.geojson.gz s3://osma/hotprojects.geojson --content-type="application/json" --content-encoding="gzip" --acl public-read-write
 rm hotprojects.geojson.gz
