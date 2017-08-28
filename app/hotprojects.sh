a8cee6804dba4b2b6c55e8351ca838ba97be0d31
rrequero <rrequero@gmail.com>
2017-08-28 15:43:10 +0200

    change bucket name

--- app/hotprojects.sh
+++ app/hotprojects.sh
@@ -7,5 +7,5 @@ rm hotprojects.raw.geojson
 
 ./hotprojects.js > hotprojects.geojson
 gzip -9 hotprojects.geojson
-aws s3 cp hotprojects.geojson.gz s3://tm-projects-vt/hotprojects.geojson --content-type="application/json" --content-encoding="gzip" --acl public-read-write
+aws s3 cp hotprojects.geojson.gz s3://osma/hotprojects.geojson --content-type="application/json" --content-encoding="gzip" --acl public-read-write
 rm hotprojects.geojson.gz
